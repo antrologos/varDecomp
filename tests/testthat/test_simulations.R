571f33cecbc99f2f5bc8b79a9c0b55eb4fbb3d02
Benjamin Elbers <elbersb@gmail.com>
2019-08-16 17:25:37 -0400

    test for symmetry

--- tests/testthat/test_simulations.R
+++ tests/testthat/test_simulations.R
@@ -9,6 +9,31 @@ setDT(wage)
 wage <- wage[wage > 5000]
 f <- log(wage) ~ racer + educr
 
+test_that("symmetric", {
+    wage1 <- wage[sample(.N, 2000), ]
+    wage2 <- wage[sample(.N, 2000), ]
+    f <- log(wage) ~ racer + educr
+
+    v1 <- varDecomp(wage1, wage2, f, silent = TRUE, precision = 1e-11)
+    v2 <- varDecomp(wage2, wage1, f, silent = TRUE, precision = 1e-11)
+
+    expect_equal(
+        v1$static[1, est_variance:obs_variance],
+        v2$static[2, est_variance:obs_variance])
+    expect_equal(
+        v1$static[2, est_variance:obs_variance],
+        v2$static[1, est_variance:obs_variance])
+    expect_equal(
+        v1$static[3, est_variance:obs_variance],
+        -v2$static[3, est_variance:obs_variance])
+    expect_equal(
+        v1$static[3, est_variance:obs_variance],
+        -v2$static[3, est_variance:obs_variance])
+    expect_equal(
+        v1$dynamic$value,
+        -v2$dynamic$value)
+})
+
 test_that("no change", {
     wage2 <- copy(wage)
     v <- varDecomp(wage, wage2, f, silent = TRUE, precision = 1e-11)
