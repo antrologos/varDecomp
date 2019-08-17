7568f0f81d77cb86db37646b679eee78a14eabd5
Benjamin Elbers <elbersb@gmail.com>
2019-08-17 12:13:03 -0400

    consistent tab size

--- R/utils.R
+++ R/utils.R
@@ -8,64 +8,64 @@ weighted.var <- function(x, w) {
 # Estimates beta and lambda coefficients
 #' @import glm2
 get_parameters <- function(data_i, formula) {
-        dep_var <- all.vars(formula[[2]])
-        indep_vars <- all.vars(formula[[3]])
+    dep_var <- all.vars(formula[[2]])
+    indep_vars <- all.vars(formula[[3]])
 
-        X <- stats::model.matrix(formula[c(1, 3)], data = data_i)
-        w <- data_i[["weight"]]
-        y <- data_i[[dep_var]]
+    X <- stats::model.matrix(formula[c(1, 3)], data = data_i)
+    w <- data_i[["weight"]]
+    y <- data_i[[dep_var]]
 
-        loglik_hist <- NULL
-        diff_step <- 1
-        iteration_count <- 0
+    loglik_hist <- NULL
+    diff_step <- 1
+    iteration_count <- 0
 
-        # Step 1 - OLS for the mean
-        reg_mean <- stats::lm.wfit(x = X, y = y, w = w)
-        e2 <- stats::residuals(reg_mean)^2
+    # Step 1 - OLS for the mean
+    reg_mean <- stats::lm.wfit(x = X, y = y, w = w)
+    e2 <- stats::residuals(reg_mean)^2
 
-        # Getting stating values for the Gamma regression
+    # Getting stating values for the Gamma regression
+    reg_var <- glm2::glm.fit2(x = X, y = e2, weights = w,
+                          family = stats::Gamma(link = "log"),
+                          control = list(maxit = 1000))
+    coef_start <- reg_var$coefficients
+
+    # Iterarative MLE  (from Western & Bloome, 2009)
+    while (diff_step > 1e-12){
+        # Step 2 - Gamma regression for the variance
         reg_var <- glm2::glm.fit2(x = X, y = e2, weights = w,
                               family = stats::Gamma(link = "log"),
+                              start  = coef_start,
                               control = list(maxit = 1000))
         coef_start <- reg_var$coefficients
+        sigma_hat <- reg_var$fitted.values
 
-        # Iterarative MLE  (from Western & Bloome, 2009)
-        while (diff_step > 1e-8){
-                # Step 2 - Gamma regression for the variance
-                reg_var <- glm2::glm.fit2(x = X, y = e2, weights = w,
-                                      family = stats::Gamma(link = "log"),
-                                      start  = coef_start,
-                                      control = list(maxit = 1000))
-                coef_start <- reg_var$coefficients
-                sigma_hat <- reg_var$fitted.values
+        # Step 3 - WLS for the mean
+        reg_mean <- stats::lm.wfit(x = X, y = y, w = w * (1 / sigma_hat))
+        e2 <- stats::residuals(reg_mean)^2
 
-                # Step 3 - WLS for the mean
-                reg_mean <- stats::lm.wfit(x = X, y = y, w = w * (1 / sigma_hat))
-                e2 <- stats::residuals(reg_mean)^2
+        # Evatuating the log-likelihood
+        Xl <- reg_var$linear.predictors
+        loglik <- sum(-.5 * (Xl + e2 * exp(-Xl)))
 
-                # Evatuating the log-likelihood
-                Xl <- reg_var$linear.predictors
-                loglik <- sum(-.5 * (Xl + e2 * exp(-Xl)))
+        loglik_hist <- c(loglik_hist, loglik)
 
-                loglik_hist <- c(loglik_hist, loglik)
-
-                iteration_count <- iteration_count + 1
-                #print(iteration_count)
-                if (iteration_count == 1){
-                        next
-                }
-
-                diff_step <- last(diff(loglik_hist))
+        iteration_count <- iteration_count + 1
+        #print(iteration_count)
+        if (iteration_count == 1){
+                next
         }
 
-        parameters <- data.table(coef = names(stats::coef(reg_mean)),
-                beta = stats::coef(reg_mean),
-                lambda = stats::coef(reg_var))
+        diff_step <- last(diff(loglik_hist))
+    }
 
-        freq <- data_i[, list(n = sum(weight)), by = indep_vars]
-        freq[, p := n / sum(n)]
+    parameters <- data.table(coef = names(stats::coef(reg_mean)),
+            beta = stats::coef(reg_mean),
+            lambda = stats::coef(reg_var))
 
-        list(parameters = parameters, freq = freq)
+    freq <- data_i[, list(n = sum(weight)), by = indep_vars]
+    freq[, p := n / sum(n)]
+
+    list(parameters = parameters, freq = freq)
 }
 
 
@@ -78,94 +78,94 @@ counterfactual_p <- function(freqs,
                              zeros_replacement = 1e-10,
                              precision = 1e-8) {
 
-        if (all(freqs[, p1 == p2])) {
-            return(freqs[["p1"]])
-        } else if (length(adjust_vars) == 0 & association_effect == FALSE) {
-            return(freqs[["p1"]])
-        } else if (length(adjust_vars) == length(indep_vars) & association_effect == TRUE) {
-            return(freqs[["p2"]])
-        }
+    if (all(freqs[, p1 == p2])) {
+        return(freqs[["p1"]])
+    } else if (length(adjust_vars) == 0 & association_effect == FALSE) {
+        return(freqs[["p1"]])
+    } else if (length(adjust_vars) == length(indep_vars) & association_effect == TRUE) {
+        return(freqs[["p2"]])
+    }
 
-        # subset
-        vars <- c(indep_vars, "p1", "p2")
-        ipf <- freqs[, ..vars]
-        ipf[p1 == 0, p1 := zeros_replacement]
-        ipf[p2 == 0, p2 := zeros_replacement]
-        ipf[, p1 := p1 / sum(p1)]
-        ipf[, p2 := p2 / sum(p2)]
+    # subset
+    vars <- c(indep_vars, "p1", "p2")
+    ipf <- freqs[, ..vars]
+    ipf[p1 == 0, p1 := zeros_replacement]
+    ipf[p2 == 0, p2 := zeros_replacement]
+    ipf[, p1 := p1 / sum(p1)]
+    ipf[, p2 := p2 / sum(p2)]
 
-        if (association_effect == TRUE) {
-            ipf[, p := p2]
+    if (association_effect == TRUE) {
+        ipf[, p := p2]
+    } else {
+        ipf[, p := p1]
+    }
+
+    # find s (source) and t (target) margins
+    for (var in indep_vars) {
+        ipf[, paste0("s_margin_", var) := sum(p), by = var]
+
+        if (var %in% adjust_vars) {
+            ipf[, paste0("t_margin_", var) := sum(p2), by = var]
         } else {
-            ipf[, p := p1]
+            ipf[, paste0("t_margin_", var) := sum(p1), by = var]
+        }
+    }
+
+    converged <- FALSE
+
+    for (i in 1:max_iterations) {
+        for (var in sample(indep_vars)) {
+            # adjust
+            t <- paste0("t_margin_", var)
+            s <- paste0("s_margin_", var)
+            ipf[, p := p * get(t) / get(s)]
+            ipf[, p := p / sum(p)]
         }
 
-        # find s (source) and t (target) margins
+        # update all margins
         for (var in indep_vars) {
             ipf[, paste0("s_margin_", var) := sum(p), by = var]
-
-            if (var %in% adjust_vars) {
-                ipf[, paste0("t_margin_", var) := sum(p2), by = var]
-            } else {
-                ipf[, paste0("t_margin_", var) := sum(p1), by = var]
-            }
         }
 
-        converged <- FALSE
+        # collect ratios
+        ratios <- sapply(indep_vars, function(var) {
+            t <- paste0("t_margin_", var)
+            s <- paste0("s_margin_", var)
+            ratio <- ipf[, list(first(get(s)), first(get(t))), by = var][, abs(V1 - V2)]
+            all(ratio <= precision)
+        })
 
-        for (i in 1:max_iterations) {
-            for (var in sample(indep_vars)) {
-                # adjust
-                t <- paste0("t_margin_", var)
-                s <- paste0("s_margin_", var)
-                ipf[, p := p * get(t) / get(s)]
-                ipf[, p := p / sum(p)]
-            }
-
-            # update all margins
-            for (var in indep_vars) {
-                ipf[, paste0("s_margin_", var) := sum(p), by = var]
-            }
-
-            # collect ratios
-            ratios <- sapply(indep_vars, function(var) {
-                t <- paste0("t_margin_", var)
-                s <- paste0("s_margin_", var)
-                ratio <- ipf[, list(first(get(s)), first(get(t))), by = var][, abs(V1 - V2)]
-                all(ratio <= precision)
-            })
-
-            if (all(ratios)) {
-                converged <- TRUE
-                break
-            }
+        if (all(ratios)) {
+            converged <- TRUE
+            break
         }
+    }
 
-        if (!converged) {
-            warning("IPF did not converge, increase max_iterations or lower precision")
-        }
+    if (!converged) {
+        warning("IPF did not converge, increase max_iterations or lower precision")
+    }
 
-        ipf$p
+    ipf$p
 }
 
 counterfactuals_simple <- function(factors, freqs) {
-        if ("mean" %in% factors)
-                mu_group <- freqs[, "mu_group2"]
-        else
-                mu_group <- freqs[, "mu_group1"]
+    if ("mean" %in% factors)
+        mu_group <- freqs[, "mu_group2"]
+    else
+        mu_group <- freqs[, "mu_group1"]
 
-        if ("var" %in% factors)
-                var <- freqs[, "var2"]
-        else
-                var <- freqs[, "var1"]
+    if ("var" %in% factors)
+        var <- freqs[, "var2"]
+    else
+        var <- freqs[, "var1"]
 
-        if ("comp" %in% factors)
-                p <- freqs[, "p2"]
-        else
-                p <- freqs[, "p1"]
+    if ("comp" %in% factors)
+        p <- freqs[, "p2"]
+    else
+        p <- freqs[, "p1"]
 
-        mu <- sum(p * mu_group)
-        sum((mu_group - mu)^2 * p) + sum(var * p)
+    mu <- sum(p * mu_group)
+    sum((mu_group - mu)^2 * p) + sum(var * p)
 }
 
 # Counterfactual function for the Shapley decomposition
@@ -173,40 +173,40 @@ counterfactuals_simple <- function(factors, freqs) {
 counterfactuals <- function(factors, indep_vars, parameters, freqs, modelmatrix,
                             mf_counterfactual_p, ...) {
 
-        if (any(str_detect(factors, "^mean_"))) {
-                factors_mean <- factors[str_detect(factors, "^mean_")]
-                factors_mean <- str_remove_all(factors_mean, "^mean_")
-                factors_mean <- paste(factors_mean, collapse = "|")
+    if (any(str_detect(factors, "^mean_"))) {
+        factors_mean <- factors[str_detect(factors, "^mean_")]
+        factors_mean <- str_remove_all(factors_mean, "^mean_")
+        factors_mean <- paste(factors_mean, collapse = "|")
 
-                cntf <- parameters[, ifelse(str_detect(coef, factors_mean), beta2, beta1)]
-                mu_group <- (modelmatrix %*% cntf)[, 1]
-        } else {
-                mu_group <- freqs[["mu_group1"]]
-        }
+        cntf <- parameters[, ifelse(str_detect(coef, factors_mean), beta2, beta1)]
+        mu_group <- (modelmatrix %*% cntf)[, 1]
+    } else {
+        mu_group <- freqs[["mu_group1"]]
+    }
 
-        if (any(str_detect(factors, "^var_"))) {
-                factors_var <- factors[str_detect(factors, "^var_")]
-                factors_var <- str_remove_all(factors_var, "^var_")
-                factors_var <- paste(factors_var, collapse = "|")
+    if (any(str_detect(factors, "^var_"))) {
+        factors_var <- factors[str_detect(factors, "^var_")]
+        factors_var <- str_remove_all(factors_var, "^var_")
+        factors_var <- paste(factors_var, collapse = "|")
 
-                cntf <- parameters[, ifelse(str_detect(coef, factors_var), lambda2, lambda1)]
-                var <- (exp(modelmatrix %*% cntf))[, 1]
-        } else {
-                var <- freqs[["var1"]]
-        }
+        cntf <- parameters[, ifelse(str_detect(coef, factors_var), lambda2, lambda1)]
+        var <- (exp(modelmatrix %*% cntf))[, 1]
+    } else {
+        var <- freqs[["var1"]]
+    }
 
-        if (any(str_detect(factors, "^comp_"))) {
-                factors_comp <- factors[str_detect(factors, "^comp_")]
-                factors_comp <- str_remove_all(factors_comp, "^comp_")
+    if (any(str_detect(factors, "^comp_"))) {
+        factors_comp <- factors[str_detect(factors, "^comp_")]
+        factors_comp <- str_remove_all(factors_comp, "^comp_")
 
-                p <- mf_counterfactual_p(freqs = freqs,
-                                         adjust_vars = factors_comp[factors_comp != "association"],
-                                         indep_vars = indep_vars,
-                                         association_effect = "association" %in% factors_comp, ...)
-        } else {
-                p <- freqs[["p1"]]
-        }
+        p <- mf_counterfactual_p(freqs = freqs,
+                                 adjust_vars = factors_comp[factors_comp != "association"],
+                                 indep_vars = indep_vars,
+                                 association_effect = "association" %in% factors_comp, ...)
+    } else {
+        p <- freqs[["p1"]]
+    }
 
-        mu <- sum(p * mu_group)
-        sum((mu_group - mu)^2 * p) + sum(var * p)
+    mu <- sum(p * mu_group)
+    sum((mu_group - mu)^2 * p) + sum(var * p)
 }
--- R/varDecomp.R
+++ R/varDecomp.R
@@ -11,156 +11,158 @@
 #' @import memoise
 #' @export
 varDecomp <- function(data1, data2, formula, weight = NULL, ...) {
-        data1 <- as.data.table(data1)
-        data2 <- as.data.table(data2)
+    data1 <- as.data.table(data1)
+    data2 <- as.data.table(data2)
 
-        # make sure that weights are always in "weight"
-        if (is.null(weight)) {
-            data1[, weight := 1]
-            data2[, weight := 1]
-        } else if (weight != "weight") {
-            data1[, weight := get(weight)]
-            data2[, weight := get(weight)]
-        }
+    # make sure that weights are always in "weight"
+    if (is.null(weight)) {
+        data1[, weight := 1]
+        data2[, weight := 1]
+    } else if (weight != "weight") {
+        data1[, weight := get(weight)]
+        data2[, weight := get(weight)]
+    }
 
-        # Getting var names
-        dep_var <- all.vars(formula[[2]])
-        indep_vars <- all.vars(formula[[3]])
+    # Getting var names
+    dep_var <- all.vars(formula[[2]])
+    indep_vars <- all.vars(formula[[3]])
 
-        # Selecting only the necessary variables
-        data1 <- data1[, c(all.vars(formula), "weight"), with = FALSE]
-        data2 <- data2[, c(all.vars(formula), "weight"), with = FALSE]
+    # Selecting only the necessary variables
+    data1 <- data1[, c(all.vars(formula), "weight"), with = FALSE]
+    data2 <- data2[, c(all.vars(formula), "weight"), with = FALSE]
 
-        # Applying transformations to y, if needed
-        data1[[dep_var]] <- data1[, eval(formula[[2]])]
-        data2[[dep_var]] <- data2[, eval(formula[[2]])]
+    # Applying transformations to y, if needed
+    data1[[dep_var]] <- data1[, eval(formula[[2]])]
+    data2[[dep_var]] <- data2[, eval(formula[[2]])]
 
-        # Removing incomplete cases
-        cases_before <- nrow(data1)
-        data1 <- data1[stats::complete.cases(data1)]
-        data1 <- data1[is.finite(data1[[dep_var]])]
-        cases_after <- nrow(data1)
-        if (cases_after < cases_before)
-            warning(paste0("Dropped ", cases_before - cases_after, " cases from data1"))
-        if (nrow(data1) == 0)
-            stop("data1 does not contain any rows")
+    # Removing incomplete cases
+    cases_before <- nrow(data1)
+    data1 <- data1[stats::complete.cases(data1)]
+    data1 <- data1[is.finite(data1[[dep_var]])]
+    cases_after <- nrow(data1)
+    if (cases_after < cases_before)
+        warning(paste0("Dropped ", cases_before - cases_after, " cases from data1"))
+    if (nrow(data1) == 0)
+        stop("data1 does not contain any rows")
 
-        cases_before <- nrow(data1)
-        data2 <- data2[stats::complete.cases(data2)]
-        data2 <- data2[is.finite(data2[[dep_var]])]
-        cases_after <- nrow(data1)
-        if (cases_after < cases_before)
-            warning(paste0("Dropped ", cases_before - cases_after, " cases from data2"))
-        if (nrow(data2) == 0)
-            stop("data2 does not contain any rows")
+    cases_before <- nrow(data1)
+    data2 <- data2[stats::complete.cases(data2)]
+    data2 <- data2[is.finite(data2[[dep_var]])]
+    cases_after <- nrow(data1)
+    if (cases_after < cases_before)
+        warning(paste0("Dropped ", cases_before - cases_after, " cases from data2"))
+    if (nrow(data2) == 0)
+        stop("data2 does not contain any rows")
 
-        # # Removing empty categories
-        for (var in indep_vars) {
-                data1[[var]] <- droplevels(data1[[var]])
-                data2[[var]] <- droplevels(data2[[var]])
+    # # Removing empty categories
+    for (var in indep_vars) {
+        data1[[var]] <- droplevels(data1[[var]])
+        data2[[var]] <- droplevels(data2[[var]])
 
-                levels1 <- paste0(levels(data1[[var]]), collapse = "|")
-                levels2 <- paste0(levels(data2[[var]]), collapse = "|")
+        levels1 <- paste0(levels(data1[[var]]), collapse = "|")
+        levels2 <- paste0(levels(data2[[var]]), collapse = "|")
 
-                if (levels1 != levels2)
-                    stop(paste0("factor levels are not identical in variable ", var))
-        }
+        if (levels1 != levels2)
+            stop(paste0("factor levels are not identical in variable ", var))
+    }
 
-        # Estimating the beta and lambda coefficients
-        model1 <- get_parameters(data1, formula)
-        model2 <- get_parameters(data2, formula)
+    # Estimating the beta and lambda coefficients
+    model1 <- get_parameters(data1, formula)
+    model2 <- get_parameters(data2, formula)
 
-        # parameters
-        setnames(model1$parameter, c("beta", "lambda"), c("beta1", "lambda1"))
-        setnames(model2$parameter, c("beta", "lambda"), c("beta2", "lambda2"))
-        # sort = FALSE is important here -- otherwise the coefficients no longer line up
-        parameters <- merge(model1$parameter, model2$parameter, by = "coef", all = TRUE,
-            sort = FALSE)
-        parameters[is.na(parameters)] <- 0
+    # parameters
+    setnames(model1$parameter, c("beta", "lambda"), c("beta1", "lambda1"))
+    setnames(model2$parameter, c("beta", "lambda"), c("beta2", "lambda2"))
+    # sort = FALSE is important here -- otherwise the coefficients no longer line up
+    parameters <- merge(model1$parameter, model2$parameter, by = "coef", all = TRUE,
+        sort = FALSE)
+    parameters[is.na(parameters)] <- 0
 
-        # cell frequencies
-        setnames(model1$freq, c("n", "p"), c("n1", "p1"))
-        setnames(model2$freq, c("n", "p"), c("n2", "p2"))
-        freqs <- merge(model1$freq, model2$freq, by = indep_vars, all = TRUE)
-        freqs[is.na(freqs)] <- 0
+    # cell frequencies
+    setnames(model1$freq, c("n", "p"), c("n1", "p1"))
+    setnames(model2$freq, c("n", "p"), c("n2", "p2"))
+    freqs <- merge(model1$freq, model2$freq, by = indep_vars, all = TRUE)
+    freqs[is.na(freqs)] <- 0
 
-        #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
-        # Variance Components
+    #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+    # Variance Components
 
-        # Basic design matrix
-        modelmatrix <- stats::model.matrix(formula[c(1, 3)], data = freqs)
+    # Basic design matrix
+    modelmatrix <- stats::model.matrix(formula[c(1, 3)], data = freqs)
 
-        # group means
-        freqs[, `:=`(
-            mu_group1 = (modelmatrix %*% parameters$beta1)[, 1],
-            mu_group2 = (modelmatrix %*% parameters$beta2)[, 1])
-        ]
-        # grand mean
-        freqs[, `:=`(
-            mu1 = sum(p1 * mu_group1),
-            mu2 = sum(p2 * mu_group2))
-        ]
-        # group variances
-        freqs[, `:=`(
-            var1 = exp(modelmatrix %*% parameters$lambda1)[, 1],
-            var2 = exp(modelmatrix %*% parameters$lambda2)[, 1])
-        ]
+    # group means
+    freqs[, `:=`(
+        mu_group1 = (modelmatrix %*% parameters$beta1)[, 1],
+        mu_group2 = (modelmatrix %*% parameters$beta2)[, 1])
+    ]
+    # grand mean
+    freqs[, `:=`(
+        mu1 = sum(p1 * mu_group1),
+        mu2 = sum(p2 * mu_group2))
+    ]
+    # group variances
+    freqs[, `:=`(
+        var1 = exp(modelmatrix %*% parameters$lambda1)[, 1],
+        var2 = exp(modelmatrix %*% parameters$lambda2)[, 1])
+    ]
 
-        #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
-        # Estimating the actual/observed variance from the components
+    #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+    # Estimating the actual/observed variance from the components
 
-        between1 <- freqs[, sum((mu_group1 - mu1)^2 * p1)]
-        within1 <- freqs[, sum(var1 * p1)]
-        est_variance1 <- between1 + within1
+    between1 <- freqs[, sum((mu_group1 - mu1)^2 * p1)]
+    within1 <- freqs[, sum(var1 * p1)]
+    est_variance1 <- between1 + within1
 
-        between2 <- freqs[, sum((mu_group2 - mu2)^2 * p2)]
-        within2 <- freqs[, sum(var2 * p2)]
-        est_variance2 <- between2 + within2
+    between2 <- freqs[, sum((mu_group2 - mu2)^2 * p2)]
+    within2 <- freqs[, sum(var2 * p2)]
+    est_variance2 <- between2 + within2
 
-        obs_variance1 <- weighted.var(data1[[dep_var]], data1[["weight"]])
-        obs_variance2 <- weighted.var(data2[[dep_var]], data2[["weight"]])
+    obs_variance1 <- weighted.var(data1[[dep_var]], data1[["weight"]])
+    obs_variance2 <- weighted.var(data2[[dep_var]], data2[["weight"]])
 
-        #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
-        # Counterfactual decomposition
+    #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+    # Counterfactual decomposition
 
-        factors_simple <- c("mean", "var", "comp")
-        decomposition_simple <- shapley(counterfactuals_simple,
-                                 factors_simple,
-                                 freqs = freqs, silent = TRUE)
-        stopifnot(all.equal(sum(decomposition_simple$value),
-            est_variance2 - est_variance1))
+    factors_simple <- c("mean", "var", "comp")
+    decomposition_simple <- shapley(counterfactuals_simple,
+                             factors_simple,
+                             freqs = freqs, silent = TRUE)
+    stopifnot(all.equal(sum(decomposition_simple$value),
+        est_variance2 - est_variance1))
 
-        factors <- list(
-            mean = paste0("mean_", indep_vars),
-            var  = c("var_Intercept", paste0("var_", indep_vars)),
-            comp = c("comp_association", paste0("comp_", indep_vars)))
+    factors <- list(
+        mean = paste0("mean_", indep_vars),
+        var  = c("var_Intercept", paste0("var_", indep_vars)),
+        comp = c("comp_association", paste0("comp_", indep_vars)))
+    print(factors)
 
-        # cache IPF results
-        mf_counterfactual_p <- memoise::memoise(counterfactual_p)
-        decomposition <- shapley(counterfactuals,
-                                 factors,
-                                 indep_vars = indep_vars,
-                                 parameters = parameters,
-                                 freqs = freqs,
-                                 modelmatrix = modelmatrix,
-                                 mf_counterfactual_p = mf_counterfactual_p, ...)
-        memoise::forget(mf_counterfactual_p)
+    # cache IPF results
+    mf_counterfactual_p <- memoise::memoise(counterfactual_p)
+    decomposition <- shapley(counterfactuals,
+                             factors,
+                             indep_vars = indep_vars,
+                             parameters = parameters,
+                             freqs = freqs,
+                             modelmatrix = modelmatrix,
+                             mf_counterfactual_p = mf_counterfactual_p, ...)
+    memoise::forget(mf_counterfactual_p)
 
-        setDT(decomposition)
-        factors <- str_split_fixed(decomposition$factor, "_", n = 2)
-        decomposition[, group := factors[, 1]]
-        decomposition[, factor := factors[, 2]]
-        decomposition[, group_value := sum(value), by = "group"]
+    setDT(decomposition)
+    factors <- str_split_fixed(decomposition$factor, "_", n = 2)
+    decomposition[, group := factors[, 1]]
+    decomposition[, factor := factors[, 2]]
+    decomposition[, group_value := sum(value), by = "group"]
 
-        stopifnot(all.equal(decomposition[, sum(value), by = "group"][, V1],
-            decomposition_simple$value))
+    stopifnot(all.equal(decomposition[, sum(value), by = "group"][, V1],
+        decomposition_simple$value))
 
-        list(static = data.table(dataset = c("1", "2", "diff"),
-                est_variance = c(est_variance1, est_variance2,
-                    est_variance2 - est_variance1),
-                est_between = c(between1, between2, between2 - between1),
-                est_within = c(within1, within2, within2 - within1),
-                obs_variance = c(obs_variance1, obs_variance2,
-                    obs_variance2 - obs_variance1)),
-             dynamic = decomposition)
+    list(parameters = parameters,
+        static = data.table(dataset = c("1", "2", "diff"),
+            est_variance = c(est_variance1, est_variance2,
+                est_variance2 - est_variance1),
+            est_between = c(between1, between2, between2 - between1),
+            est_within = c(within1, within2, within2 - within1),
+            obs_variance = c(obs_variance1, obs_variance2,
+                obs_variance2 - obs_variance1)),
+         dynamic = decomposition)
 }
