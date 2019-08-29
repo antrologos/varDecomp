b6a9b5c28fc928dfa30abd6623b2b2eab31c8ee6
Rogerio Jeronimo Barbosa <antrologos@gmail.com>
2019-08-29 16:40:08 -0300

    new tests for the composition effect

--- tests/testthat/test_simulations.R
+++ tests/testthat/test_simulations.R
@@ -107,7 +107,6 @@ test_that("var: Intercept", {
     expect_equal(values_for_testing$value, values_for_testing$group_value)
 })
 
-
 test_that("var: educr", {
     # increase variance in earnings without affecting the mean
 
@@ -236,45 +235,224 @@ test_that("var: racer", {
 })
 
 
-
 test_that("comp: educr", {
-    # increase in college graduation (those with college have higher variance)
-    wage[, wt := 1]
 
-    wage2 <- copy(wage)
-    wage2[educr == "4-year college+", wt := 4]
+    # Simulates new sample weights (changing the marginals of racer,
+    # but keeping  the statistical association and the marginals for education identical)
+    ipf <- wage[, .N, by = c("educr", "racer")]
 
-    v <- varDecomp(wage, wage2, f, weight = "wt", silent = TRUE, precision = 1e-15)
+    ipf[, p0 := N/sum(N)] # Original joint distribution
 
-    # static and dynamic match
-    expect_equal(v$static$est_variance[[3]], sum(v$dynamic$value))
+    # The source margins are obtained from the simulated distribution
+    ipf[, s_margin_racer := sum(p0), by = racer]
+    ipf[, s_margin_educr := sum(p0), by = educr]
 
-    # change is mostly explained by comp educr effect, i.e. all others are close to 0
-    expect_gt(v$dynamic[group == "comp" & factor == "educr", value] / v$static$est_variance[[3]],
-        .99)
+    # The target margins for educt are the same
+    ipf[, t_margin_racer := sum(p0), by = racer]
+
+    # ... but the target margins for racer are not
+    ipf[, t_margin_educr := sum(p0), by = educr]
+    ipf[ educr == "4-year college+", t_margin_educr := 3*t_margin_educr]
+    ipf[, t_margin_educr := t_margin_educr/sum(t_margin_educr), by=racer]
+
+    # Marginal distributions (for further comparison)
+    m_educ0 = ipf[, .(p = sum(p0)), by = educr]
+    m_race0 = ipf[, .(p = sum(p0)), by = racer]
+
+    # IPF for getting a distribution with same margins as the original, but
+    # with same the statistical association as the simulated one
+    indep_vars = c("educr", "racer")
+    ipf[, p1 := p0]
+    for(i in 1:40) {
+
+        for (var in indep_vars) {
+            # adjust
+            t <- paste0("t_margin_", var)
+            s <- paste0("s_margin_", var)
+            ipf[, p1 := p1*(get(t) / get(s))]
+            ipf[, p1 := p1 / sum(p1)]
+        }
+
+        # update all margins
+        for (var in indep_vars) {
+            ipf[, paste0("s_margin_", var) := sum(p1), by = var]
+        }
+    }
+
+    # Odds ratio (for comparison)
+    t0 <- tidyr::spread(ipf[, .(educr, racer, p0)], key = racer, value=p0)
+    t1 <- tidyr::spread(ipf[, .(educr, racer, p1)], key = racer, value=p1)
+    p0_odds <- vcd::loddsratio(as.matrix(t0[, -1]))$coefficients
+    p1_odds <- vcd::loddsratio(as.matrix(t1[, -1]))$coefficients
+
+    # the odds ratios are the same
+    expect_equal(p0_odds, p1_odds)
+
+    # the joint distribution changed
+    expect_false(all(ipf$p0 == ipf$p1))
+
+    # the marginal distribution for race are the same
+    m_race1 = ipf[, .(p = sum(p1)), by = racer]
+    expect_equal(m_race0, m_race1)
+
+    # the marginal distribution for education changed
+    m_educ1 = ipf[, .(p = sum(p1)), by = educr]
+    expect_false(all(round(m_educ0[,2], 10) == round(m_educ1[,2], 10)))
+
+    # NEW SAMPLE WEIGHTS!
+    ipf[ , wt := (p1*sum(N))/N]
+    setDT(ipf, key = c("racer", "educr"))
+
+    # Merging with the original microdata
+    wage$wt = NULL
+    wage2 = copy(wage)
+    setDT(wage2, key = c("racer", "educr"))
+    wage2[ipf, wt := wt]
+
+    # Frequency table for the new dataset, with simulated weights (for comparison)
+    t2 <- tidyr::spread(wage2[, .(n = sum(wt)), by = c("educr", "racer")], key = racer, value=n)
+
+    # Sorting values (using DT keys)
+    setDT(t0, c("educr", "racer"))
+    setDT(t1, c("educr", "racer"))
+    setDT(t2, c("educr", "racer"))
+
+    t0 <- prop.table(t0[, -1])
+    t1 <- prop.table(t1[, -1])
+    t2 <- prop.table(t2[, -1])
+
+    # the dataset wage2 must have the same marginal distribution for education as the simulated dataset
+    expect_false(all(rowSums(t2) ==rowSums(t0)))
+
+    # the dataset wage2 must have the same marginal distribution for race as the original data
+    expect_equal(colSums(t2), colSums(t0))
+
+    # the dataset wage2 must have the same odds ratio as the original distribution
+    expect_equal(vcd::loddsratio(as.matrix(t0))$coefficients, vcd::loddsratio(as.matrix(t2))$coefficients)
+
+    # the dataset wage2 must have the same odds ratio as the simulated distribution
+    expect_equal(vcd::loddsratio(as.matrix(t1))$coefficients, vcd::loddsratio(as.matrix(t2))$coefficients)
+
+    wage$wt = 1
+    v <- varDecomp(wage, wage2, f, weight = "wt", precision = 1e-14)
+
+    values_for_testing <- v$dynamic[group == "comp" & factor == "educr"]
+
+    expect_equal(values_for_testing$value, values_for_testing$group_value)
+
 })
 
 
 test_that("comp: racer", {
-    # increase in Black population (have less variance)
-    wage[, wt := 1]
-    wage2 <- copy(wage)
-    wage2[racer == "White", wt := 5]
 
-    v <- varDecomp(wage, wage2, f, weight = "wt", silent = TRUE, precision = 1e-14)
+    # Simulates new sample weights (changing the marginals of racer,
+    # but keeping  the statistical association and the marginals for education identical)
+    ipf <- wage[, .N, by = c("educr", "racer")]
 
-    # static and dynamic match
-    expect_equal(v$static$est_variance[[3]], sum(v$dynamic$value))
+    ipf[, p0 := N/sum(N)] # Original joint distribution
 
-    # change is only explained by comp racer effect, i.e. all others are 0
-    # TODO: --> this doesn't work as nicely as does comp: educr
-    expect_gt(v$dynamic[group == "comp" & factor == "racer", value] / v$static$est_variance[[3]],
-        .99)
+    # The source margins are obtained from the simulated distribution
+    ipf[, s_margin_racer := sum(p0), by = racer]
+    ipf[, s_margin_educr := sum(p0), by = educr]
+
+    # The target margins for educt are the same
+    ipf[, t_margin_educr := sum(p0), by = educr]
+
+    # ... but the target margins for racer are not
+    ipf[, t_margin_racer := sum(p0), by = racer]
+    ipf[ racer == "Black", t_margin_racer := 3*t_margin_racer]
+    ipf[, t_margin_racer := t_margin_racer/sum(t_margin_racer), by=educr]
+
+    # Marginal distributions (for further comparison)
+    m_educ0 = ipf[, .(p = sum(p0)), by = educr]
+    m_race0 = ipf[, .(p = sum(p0)), by = racer]
+
+    # IPF for getting a distribution with same margins as the original, but
+    # with same the statistical association as the simulated one
+    indep_vars = c("educr", "racer")
+    ipf[, p1 := p0]
+    for(i in 1:40) {
+
+        for (var in indep_vars) {
+            # adjust
+            t <- paste0("t_margin_", var)
+            s <- paste0("s_margin_", var)
+            ipf[, p1 := p1*(get(t) / get(s))]
+            ipf[, p1 := p1 / sum(p1)]
+        }
+
+        # update all margins
+        for (var in indep_vars) {
+            ipf[, paste0("s_margin_", var) := sum(p1), by = var]
+        }
+    }
+
+    # Odds ratio (for comparison)
+    t0 <- tidyr::spread(ipf[, .(educr, racer, p0)], key = racer, value=p0)
+    t1 <- tidyr::spread(ipf[, .(educr, racer, p1)], key = racer, value=p1)
+    p0_odds <- vcd::loddsratio(as.matrix(t0[, -1]))$coefficients
+    p1_odds <- vcd::loddsratio(as.matrix(t1[, -1]))$coefficients
+
+    # the odds ratios are the same
+    expect_equal(p0_odds, p1_odds)
+
+    # the joint distribution changed
+    expect_false(all(ipf$p0 == ipf$p1))
+
+    # the marginal distribution for race changed
+    m_race1 = ipf[, .(p = sum(p1)), by = racer]
+    expect_false(all(round(m_race0[,2], 10) == round(m_race1[,2], 10)))
+
+    # the marginal distribution for education is the same
+    m_educ1 = ipf[, .(p = sum(p1)), by = educr]
+    expect_equal(m_educ0, m_educ1)
+
+    # NEW SAMPLE WEIGHTS!
+    ipf[ , wt := (p1*sum(N))/N]
+    setDT(ipf, key = c("racer", "educr"))
+
+    # Merging with the original microdata
+    wage$wt = NULL
+    wage2 = copy(wage)
+    setDT(wage2, key = c("racer", "educr"))
+    wage2[ipf, wt := wt]
+
+    # Frequency table for the new dataset, with simulated weights (for comparison)
+    t2 <- tidyr::spread(wage2[, .(n = sum(wt)), by = c("educr", "racer")], key = racer, value=n)
+
+    # Sorting values (using DT keys)
+    setDT(t0, c("educr", "racer"))
+    setDT(t1, c("educr", "racer"))
+    setDT(t2, c("educr", "racer"))
+
+    t0 <- prop.table(t0[, -1])
+    t1 <- prop.table(t1[, -1])
+    t2 <- prop.table(t2[, -1])
+
+    # the dataset wage2 must have the same marginal distribution for race as the simulated data
+    expect_false(all(colSums(t2) == colSums(t0)))
+
+    # the dataset wage2 must have the same marginal distribution for education as the original data
+    expect_equal(rowSums(t2), rowSums(t0))
+
+    # the dataset wage2 must have the same odds ratio as the original distribution
+    expect_equal(vcd::loddsratio(as.matrix(t0))$coefficients, vcd::loddsratio(as.matrix(t2))$coefficients)
+
+    # the dataset wage2 must have the same odds ratio as the simulated distribution
+    expect_equal(vcd::loddsratio(as.matrix(t1))$coefficients, vcd::loddsratio(as.matrix(t2))$coefficients)
+
+    wage$wt = 1
+    v <- varDecomp(wage, wage2, f, weight = "wt", precision = 1e-14)
+
+    values_for_testing <- v$dynamic[group == "comp" & factor == "racer"]
+
+    expect_equal(values_for_testing$value, values_for_testing$group_value)
+
 })
 
 
-test_that("comp: association", {
 
+test_that("comp: association", {
 
     # Simulates new sample weights (keeping the original marginals, but changing the statistical association)
 
